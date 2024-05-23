c698c5cb35cda762c0f1adc96973262efc908cc3
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-05-23 20:09:10 +0000

    add Dashboard

---
+++ frontend/components/SideNavbar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import { Nav } from "./ui/nav";
+
+type Props = {};
+
+import {
+  ShoppingCart,
+  LayoutDashboard,
+  UsersRound,
+  Settings,
+  ChevronRight,
+} from "lucide-react";
+import { Button } from "./ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+
+import { useWindowWidth } from "@react-hook/window-size";
+
+export default function SideNavbar({}: Props) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onlyWidth = useWindowWidth();
+  const mobileWidth = onlyWidth < 768;
+
+  function toggleSidebar() {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  return (
+    <div className="relative flex flex-col items-center justify-between border-r lg:px-3 pt-20 transition-all duration-300 ease-in-out">
+      {!mobileWidth && (
+        <div className="absolute right-[-20px] top-7">
+          <Button
+            onClick={toggleSidebar}
+            variant="secondary"
+            className="rounded-full p-2"
+          >
+            <ChevronRight
+              className={`transition-transform duration-300 ease-in-out ${isCollapsed ? "rotate-180" : "rotate-0"}`}
+            />
+          </Button>
+        </div>
+      )}
+      <Nav
+        isCollapsed={mobileWidth ? true : isCollapsed}
+        links={[
+          {
+            title: "Dashboard",
+            href: "/",
+            icon: LayoutDashboard,
+            variant: "default",
+          },
+          {
+            title: "Users",
+            href: "/users",
+            icon: UsersRound,
+            variant: "ghost",
+          },
+          {
+            title: "Ordrs",
+            href: "/orders",
+            icon: ShoppingCart,
+            variant: "ghost",
+          },
+          {
+            title: "Settings",
+            href: "/settings",
+            icon: Settings,
+            variant: "ghost",
+          },
+        ]}
+      />
+      <ModeToggle />
+    </div>
+  );
+}
