de6db609e16910e2453afb687996e8e71b0e038e
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-05-29 15:42:05 +0000

    Fix Dashboard issue

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,22 +2,8 @@
 
 import PageTitle from "@/components/PageTitle";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { projects } from "@/data/projects";
 import React from "react";
-
-export const projects = [
-  {
-    title: "Event Booking",
-    description:
-      "A platform that allows users to book events, concerts, and shows.",
-    link: "/dashboard/events",
-  },
-  {
-    title: "Expense Tracker",
-    description:
-      "A platform that allows users to track their expenses and manage their finances.",
-    link: "https://netflix.com",
-  },
-];
 
 export default function Dashboard() {
   return (
---
+++ frontend/data/projects.ts
@@ -0,0 +1,15 @@
+// projects.ts
+export const projects = [
+  {
+    title: "Event Booking",
+    description:
+      "A platform that allows users to book events, concerts, and shows.",
+    link: "/dashboard/events",
+  },
+  {
+    title: "Expense Tracker",
+    description:
+      "A platform that allows users to track their expenses and manage their finances.",
+    link: "https://netflix.com",
+  },
+];
