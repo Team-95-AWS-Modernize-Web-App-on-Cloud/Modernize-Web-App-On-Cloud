c4e41f816bfa161ff1b7d5206658d12738a1fc24
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-05-30 09:58:21 +0700

    create a function in EventRepository to check if the event with this eventName, and eventTime already exists in Event table

--- Backend/Event_Booking/src/main/java/com/project/Event_Booking/Repository/EventRepository.java
+++ Backend/Event_Booking/src/main/java/com/project/Event_Booking/Repository/EventRepository.java
@@ -3,5 +3,9 @@ package com.project.Event_Booking.Repository;
 import com.project.Event_Booking.Entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface EventRepository extends JpaRepository<Event, Integer> {
+
+    boolean existsByEventNameAndEventTime(String eventName, LocalDateTime eventTime);
 }
